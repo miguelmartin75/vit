77ebd578f35090dd34a1ee8d01c521ad8d5d77dc
Miguel Martin <miguelmartin@fb.com>
2025-01-26 02:24:25 +0000

    tensor_view + zeros

--- vit.cpp
+++ vit.cpp
@@ -1,18 +1,17 @@
 #include "vit.h"
 
+#include <stdlib.h>
+#include <string.h>
+
 /*
 TODO:
 - [ ] 
  */
 
-struct ViT_CPU {
-    void forward(Tensor input) {
-    }
-    Tensor backward() {
-    }
-};
-
 // CPU
+struct ViT_CPU {
+    // TODO
+};
 
 // Tensor rand3(u32 c, u32 h, u32, w) {
 //     // TODO
@@ -22,29 +21,139 @@ struct ViT_CPU {
 //     // TODO
 // }
 
-Tensor zeros3(u32 c, u32 h, u32, w) {
+// TODO: 
+// Tensor operations:
+//  - initialization: randn, zeros
+//  - concat
+//  - slice
+
+i64 shape_nelem(Shape shape) {
+    i64 n = shape.n > 0;
+    for(int i = 0; i < shape.n; ++i) {
+        n *= shape.dims[i];
+    }
+    return n;
+}
+
+void shape_autofill(Shape* shape, Shape ref) {
+    // TODO: return errors?
+    i64 n = shape_nelem(ref);
+    i64 n_fill_idx = -1;
+    for(i32 i = 0; i < shape->n; ++i) {
+        if(shape->dims[i] == -1) {
+            VIT_ASSERT(n_fill_idx == -1, "only 1 dim can be auto-filled");
+            n_fill_idx = i;
+        } else {
+            // TODO: return errors?
+            VIT_ASSERT(n % shape->dims[i] == 0, "non-divisble shape");
+            n /= shape->dims[i];
+        }
+    }
+    if(n_fill_idx > -1) {
+        shape->dims[n_fill_idx] = n;
+    }
+}
+
+Tensor tensor_view(Tensor x, Shape shape) {
+    shape_autofill(&shape, x.shape);
+    u64 lhs = shape_nelem(x.shape);
+    u64 rhs = shape_nelem(shape);
+    // TODO: return errors?
+    VIT_ASSERT(lhs == rhs, "shapes incompat");
+    VIT_ASSERT(rhs > 0, "shape must not contain negative dims");
+    return Tensor{
+        // .dtype = x.dtype,
+        .data = x.data,
+        .shape = shape
+    };
+}
+
+Tensor tensor_concat(Tensor a, Tensor b, bool inplaceA = false) {
     // TODO
 }
 
-Tensor zeros4(u32 b, u32 c, u32 h, u32, w) {
+Tensor tensor_add(Tensor a, Tensor b) {
     // TODO
+}
+
+Tensor tensor_mean(Tensor x, i32 dim) {
+    // TODO
+}
+
+Tensor tensor_variance(Tensor x, i32 dim) {
+    // TODO
+}
+
+Tensor tensor_zeros_cpu(Shape shape) {
+    // TODO: use allocator
+    i64 nelem = shape_nelem(shape);
+    byte8* mem = (byte8*)calloc(sizeof(u32) * shape.n + nelem * sizeof(f32), 1);
+    byte8* data = mem + sizeof(u32)*4;
+    Shape shape_copy{.n = shape.n, .dims = (i32*)mem};
+    memcpy(shape_copy.dims, shape.dims, sizeof(i32) * shape.n);
+
+    return Tensor{
+        // TODO device / dtype
+        .data = data,
+        .shape = shape_copy
+    };
 }
 
 ViT vit_init(ViTModelParams params) {
+    // TODO: switch on device
+    ViT ret;
+    ret.ctx = new ViT_CPU;
+    return ret;
 }
-void vit_destroy(ViT*) {
+
+void vit_destroy(ViT* vit) {
+    delete (ViT_CPU*)vit->ctx;
+    vit->ctx = nullptr;
 }
+
 void vit_forward(ViT*, Tensor input) {
+    // TODO: batched
+    // Tensor x = tensor_view(input, shape_lit({-1, 16*16*3}));
+    // 1. [ ] slice input into 16x16 patches
+    //    - C W H -> N X (3*16*16)
+    // 2. [ ] feed into MLP (two-layers)
+    // 3. [ ] concat with position embeddings
+    // 4. [ ] feed into L layers of 
+
 }
+
 Tensor vit_backward(ViT*) {
 }
 
+// TODO: move
+void test_shape() {
+    Shape shape = shape_lit({-1, 16, 16});
+    VIT_ASSERT(shape_nelem(shape) == -256);
+
+    Shape ref = shape_lit({3, 224, 224});
+    shape_autofill(&shape, ref);
+    VIT_ASSERT(shape.dims[0] == 588);
+}
+
+void test_tensor_init() {
+    Tensor zeros = tensor_zeros_cpu(shape_lit({3, 224, 224}));
+    VIT_ASSERT(shape_nelem(zeros.shape) == 150528);
+
+    Tensor new_view = tensor_view(zeros, shape_lit({-1, 16*16}));
+    VIT_ASSERT(shape_nelem(new_view.shape) == 150528);
+    VIT_ASSERT(new_view.shape.dims[0] == 588);
+}
+
 int main() {
+    test_shape();
+    test_tensor_init();
+
     ViT vit = vit_init(ViTModelParams{
-        .device = DEVICE_CPU,
+        // TODO
+        // .device = DEVICE_CPU,
         .patch_size = 16
     });
 
-    Tensor tensor = zeros4(64, 3, 224, 224);
-    // vit_forward_cpu();
+    Tensor inp = tensor_zeros_cpu(shape_lit({3, 224, 224}));
+    vit_forward(&vit, inp);
 }
--- vit.h
+++ vit.h
@@ -6,8 +6,13 @@
 extern "C" {
 #endif
 
+// TODO: move to _internal ?
 #define VIT_INTERNAL static
-#define VIT_ASSERT(x, ...) if(x) __builtin_debugtrap()
+// #define VIT_ASSERT(x, ...) if(x) __builtin_debugtrap()
+#define VIT_ASSERT(x, ...) if(!(x)) __builtin_trap()
+
+#define countof(a)    (u64)(sizeof(a) / sizeof(*(a)))
+#define lengthof(s)   (countof(s) - 1)
 
 typedef uint8_t byte8;
 typedef float f32;
@@ -21,26 +26,41 @@ typedef uint64_t u64;
 typedef int64_t ll;
 typedef int32_t rune;
 
-
 typedef enum {
     DEVICE_CPU,
     DEVICE_CUDA,
     // TODO SYLC
+} DeviceKind;
+
+typedef struct {
+    DeviceKind kind : 8;
+    u32 idx : 24;
 } Device;
 
 typedef struct {
-    Device device;
+    // TODO
+    // Device device;
     int patch_size;
 } ViTModelParams;
 
 typedef struct {
-    Device device;
-    void* data;
+    // TODO
+    // Device device;
+    void* ctx;
 } ViT;
 
 typedef struct {
-    float* data;
-    uint32_t* shape;
+    i32 rw; // read-only
+    i32 n;
+    i32* dims;
+} Shape;
+
+typedef struct {
+    // TODO
+    // Device device;
+    // DType dtype;
+    byte8* data;
+    Shape shape;
 } Tensor;
 
 ViT vit_init(ViTModelParams params);
@@ -48,7 +68,38 @@ void vit_destroy(ViT*);
 void vit_forward(ViT*, Tensor input);
 Tensor vit_backward(ViT*);
 
+
 #ifdef  __cplusplus
 }
 #endif
 
+#include <algorithm>
+
+template<i32 N>
+Shape shape_lit(const i32 (&dims)[N]) {
+    // TODO: allocator
+    i32* data = new i32[N];
+    // TODO: remove <algorithm> usage
+    std::copy(dims, dims + N, data);
+    return Shape{.rw = 0, .n = N, .dims = data};
+}
+
+#if 1  // TODO: move to impl?
+
+#include <fmt/core.h>
+#include <fmt/ranges.h>
+
+// https://github.com/fmtlib/fmt/issues/2134
+template <typename... T>
+void print(fmt::format_string<T...> fmt, T&&... args) {
+    fmt::print(fmt, std::forward<T>(args)...);
+    fflush(stdout);
+}
+
+template <typename... T>
+void println(fmt::format_string<T...> fmt, T&&... args) {
+    auto str = fmt::format(fmt, std::forward<T>(args)...);
+    print("{}\n", str);
+}
+
+#endif
